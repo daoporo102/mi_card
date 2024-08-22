29c48b819b3ecf54a0ecd5e191e2cbf4f59b45d4
Truong Quang Dao <114506148+daoporo102@users.noreply.github.com>
2024-08-22 22:55:41 +0700

    Mi card app using Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+   MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/dao.jpg'),
+              ),
+              Text('DaoTq',style: TextStyle(fontFamily: 'Pacifico',fontSize: 40,color: Colors.white)),
+              Text('FLUTTER DEVELOPER',style: TextStyle(fontFamily: 'SourceCodePro',fontSize: 20,letterSpacing: 1,fontWeight: FontWeight.bold,color: Colors.teal.shade100)),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(color: Colors.teal.shade100,),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 30),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading:  Icon(Icons.phone,size: 30,color: Colors.teal,),
+                    title: Text('+84 343 600 222',style: TextStyle(color: Colors.teal.shade900,fontFamily: 'SourceCodePro',fontSize: 20),) ,
+                  )
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 30),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email,color: Colors.teal,),
+                    title: Text('daotq.21it@vku.udn.vn',style: TextStyle(color: Colors.teal.shade900,fontFamily: 'SourceCodePro',fontSize: 20),),
+                  )
+                ),
+              )
+            ],
+          )
+        ),
+        
       ),
-    ),
-  );
+      debugShowCheckedModeBanner: false,
+    );
+     
+  }
 }
